3b08fcdab75676aabb8b1f589f6afd2e036a154b
easyhooon <51016231+easyhooon@users.noreply.github.com>
2021-06-14 11:00:01 +0900

    Map 관련 밑작업 세팅

---
+++ app/src/main/java/kr/ac/konkuk/koogle/Key.kt
@@ -0,0 +1,5 @@
+package kr.ac.konkuk.koogle
+
+object Key {
+    const val TMAP_API = "l7xxdf667b4fe39b4e44be1c1f105093e906"
+}
---
+++ app/src/main/java/kr/ac/konkuk/koogle/Model/Entity/SearchResultEntity.kt
@@ -0,0 +1,11 @@
+package kr.ac.konkuk.koogle.Model.Entity
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class SearchResultEntity (
+    val fullAddress: String,
+    val name: String,
+    val locationLatLng: LocationLatLngEntity
+): Parcelable
